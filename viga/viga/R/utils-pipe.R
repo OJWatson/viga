7e6865611161bd5fcf4b70c6f1c4f80ca6c1ecd8
OJWatson <o.watson15@imperial.ac.uk>
2025-07-01 17:01:20 +0100

    first commit

---
+++ viga/viga/R/utils-pipe.R
@@ -0,0 +1,27 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
+NULL
+
+
+#' Bind list of data frames together
+#'
+#' @param x List of data.frames to bind together
+rbind_list_base <- function(x) {
+  x2 <- do.call(
+    rbind.data.frame,
+    c(x, stringsAsFactors = FALSE, make.row.names = FALSE)
+  )
+  rownames(x2) <- seq_len(dim(x2)[1])
+  x2
+}
